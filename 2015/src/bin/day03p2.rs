65003b54f5ec9ecdc500f7b5de21eaa13cc6515f
Manuel Hutter <manuel@hutter.io>
2021-11-11 19:06:02 +0100

    2015: Day 3 Part 1
    Signed-off-by: Manuel Hutter <manuel@hutter.io>

--- 2015/src/bin/day03p2.rs
+++ 2015/src/bin/day03p2.rs
@@ -3,6 +3,6 @@ use common::generate;
 use advent_of_code_2015::day03::day03p2;
 
 fn main() {
-    let i = generate::ints(include_str!("../../input/day03.txt"));
+    let i = generate::chars(include_str!("../../input/day03.txt"));
     dbg!(day03p2(&i));
 }
--- 2015/src/day03.rs
+++ 2015/src/day03.rs
@@ -1,18 +1,39 @@
-pub fn day03p1(input: &[i64]) -> i64 {
-    input.len() as i64
+use std::collections::HashSet;
+
+pub fn day03p1(input: &[char]) -> i64 {
+    let (mut x, mut y) = (0, 0);
+    let mut visited = HashSet::new();
+    visited.insert((x, y));
+
+    for c in input {
+        match c {
+            '^' => y += 1,
+            'v' => y -= 1,
+            '>' => x += 1,
+            '<' => x -= 1,
+            _ => panic!("unexpected input: {}", c),
+        }
+        visited.insert((x, y));
+    }
+
+    visited.len() as i64
 }
 
-pub fn day03p2(input: &[i64]) -> i64 {
+pub fn day03p2(input: &[char]) -> i64 {
     input.len() as i64
 }
 
 #[cfg(test)]
 mod tests {
+    use common::generate::chars;
+
     use super::*;
 
     #[test]
     fn part1_examples() {
-        assert_eq!(0, day03p1(&[]))
+        assert_eq!(2, day03p1(&chars(">")));
+        assert_eq!(4, day03p1(&chars("^>v<")));
+        assert_eq!(2, day03p1(&chars("^v^v^v^v^v")));
     }
 
     #[test]
