a0b38a032f07a8de93fa8d932d1a5aefee53b07d
Manuel Hutter <manuel@hutter.io>
2021-11-11 15:24:14 +0100

    2015: Day 1
    Signed-off-by: Manuel Hutter <manuel@hutter.io>

--- 2015/src/bin/day01p2.rs
+++ 2015/src/bin/day01p2.rs
@@ -3,6 +3,6 @@ use common::generate;
 use advent_of_code_2015::day01::day01p2;
 
 fn main() {
-    let i = generate::ints(include_str!("../../input/day01.txt"));
+    let i = generate::chars(include_str!("../../input/day01.txt"));
     dbg!(day01p2(&i));
 }
--- 2015/src/day01.rs
+++ 2015/src/day01.rs
@@ -1,22 +1,59 @@
-pub fn day01p1(input: &[i64]) -> i64 {
-    input.len() as i64
+pub fn day01p1(input: &[char]) -> i64 {
+    let mut floor = 0;
+
+    for c in input {
+        match c {
+            '(' => floor += 1,
+            ')' => floor -= 1,
+            _ => panic!("unexpected input: {}", c),
+        }
+    }
+
+    floor
 }
 
-pub fn day01p2(input: &[i64]) -> i64 {
-    input.len() as i64
+pub fn day01p2(input: &[char]) -> i64 {
+    let mut floor = 0;
+    let mut pos = 0;
+
+    for c in input {
+        pos += 1;
+        match c {
+            '(' => floor += 1,
+            ')' => floor -= 1,
+            _ => panic!("unexpected input: {}", c),
+        }
+
+        if floor < 0 {
+            return pos;
+        }
+    }
+
+    panic!("did not enter basement")
 }
 
 #[cfg(test)]
 mod tests {
+    use common::generate::chars;
+
     use super::*;
 
     #[test]
     fn part1_examples() {
-        assert_eq!(0, day01p1(&[]))
+        assert_eq!(0, day01p1(&chars("(())")));
+        assert_eq!(0, day01p1(&chars("()()")));
+        assert_eq!(3, day01p1(&chars("(((")));
+        assert_eq!(3, day01p1(&chars("(()(()(")));
+        assert_eq!(3, day01p1(&chars("))(((((")));
+        assert_eq!(-1, day01p1(&chars("())")));
+        assert_eq!(-1, day01p1(&chars("))(")));
+        assert_eq!(-3, day01p1(&chars(")))")));
+        assert_eq!(-3, day01p1(&chars(")())())")));
     }
 
     #[test]
     fn part2_examples() {
-        assert_eq!(0, day01p2(&[]))
+        assert_eq!(1, day01p2(&chars(")")));
+        assert_eq!(5, day01p2(&chars("()())")));
     }
 }
