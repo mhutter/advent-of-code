18861e479b5aabc626234344bb2c00fbdc6ba26c
Manuel Hutter <manuel@hutter.io>
2021-12-01 07:44:09 +0100

    2021: Day 01 Part 1
    Signed-off-by: Manuel Hutter <manuel@hutter.io>

--- 2021/src/day01.rs
+++ 2021/src/day01.rs
@@ -1,5 +1,16 @@
 pub fn day01p1(input: &[i32]) -> i32 {
-    input.len() as i32
+    let mut increases = 0;
+    let mut iter = input.iter();
+    let mut previous = iter.next().unwrap();
+
+    for next in iter {
+        if next > previous {
+            increases += 1;
+        }
+        previous = next;
+    }
+
+    increases
 }
 
 pub fn day01p2(input: &[i32]) -> i32 {
@@ -12,7 +23,10 @@ mod tests {
 
     #[test]
     fn part1_examples() {
-        assert_eq!(0, day01p1(&[]));
+        assert_eq!(
+            7,
+            day01p1(&[199, 200, 208, 210, 200, 207, 240, 269, 260, 263])
+        );
     }
 
     #[test]
