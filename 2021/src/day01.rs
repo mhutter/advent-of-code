97cdb5e1c6a42b8ec93d8818261d40acfd50d59d
Manuel Hutter <manuel@hutter.io>
2021-12-01 08:09:29 +0100

    2021: Day 01 Part 2
    Signed-off-by: Manuel Hutter <manuel@hutter.io>

--- 2021/src/day01.rs
+++ 2021/src/day01.rs
@@ -14,23 +14,37 @@ pub fn day01p1(input: &[i32]) -> i32 {
 }
 
 pub fn day01p2(input: &[i32]) -> i32 {
-    input.len() as i32
+    let mut increases = 0;
+    let mut previous = input[0] + input[1] + input[2];
+
+    for i in 0..input.len() {
+        if i < 3 {
+            continue;
+        }
+
+        let sum = input[i] + input[i - 1] + input[i - 2];
+        if sum > previous {
+            increases += 1;
+        }
+        previous = sum;
+    }
+
+    increases
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
+    const INPUT: &[i32] = &[199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
+
     #[test]
     fn part1_examples() {
-        assert_eq!(
-            7,
-            day01p1(&[199, 200, 208, 210, 200, 207, 240, 269, 260, 263])
-        );
+        assert_eq!(7, day01p1(INPUT));
     }
 
     #[test]
     fn part2_examples() {
-        assert_eq!(0, day01p2(&[]));
+        assert_eq!(5, day01p2(INPUT));
     }
 }
