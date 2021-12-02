2e9946acef74c29b15fd71b2b04fd5cc168c0cbd
Manuel Hutter <manuel@hutter.io>
2021-12-02 09:15:12 +0100

    2021: Day 02 Part 1
    Signed-off-by: Manuel Hutter <manuel@hutter.io>

--- 2021/src/bin/day02p1.rs
+++ 2021/src/bin/day02p1.rs
@@ -1,8 +1,6 @@
-use common::generate;
-
 use advent_of_code_2021::day02::day02p1;
 
 fn main() {
-    let i = generate::ints(include_str!("../../input/day02.txt"));
+    let i = include_str!("../../input/day02.txt");
     dbg!(day02p1(&i));
 }
--- 2021/src/day02.rs
+++ 2021/src/day02.rs
@@ -1,8 +1,24 @@
-pub fn day02p1(input: &[i32]) -> i32 {
-    input.len() as i32
+pub fn day02p1(input: &str) -> i32 {
+    let mut pos = 0;
+    let mut depth = 0;
+
+    for line in input.lines() {
+        let mut parts = line.split(' ');
+        let direction = parts.next().unwrap();
+        let amount: i32 = parts.next().unwrap().parse().unwrap();
+
+        match direction {
+            "forward" => pos += amount,
+            "down" => depth += amount,
+            "up" => depth -= amount,
+            _ => panic!("unexpected direction {}", direction),
+        }
+    }
+
+    pos * depth
 }
 
-pub fn day02p2(input: &[i32]) -> i32 {
+pub fn day02p2(input: &str) -> i32 {
     input.len() as i32
 }
 
@@ -10,13 +26,15 @@ pub fn day02p2(input: &[i32]) -> i32 {
 mod tests {
     use super::*;
 
+    const INPUT: &str = "forward 5\ndown 5\nforward 8\nup 3\ndown 8\nforward 2\n";
+
     #[test]
     fn part1_examples() {
-        assert_eq!(0, day02p1(&[]));
+        assert_eq!(150, day02p1(INPUT));
     }
 
     #[test]
     fn part2_examples() {
-        assert_eq!(0, day02p2(&[]));
+        assert_eq!(0, day02p2(INPUT));
     }
 }
