4c84b92a72d55cf0264ddcfe23ff88722d93080f
Manuel Hutter <manuel@hutter.io>
2021-12-06 20:33:16 +0100

    Add generate::int_list
    Signed-off-by: Manuel Hutter <manuel@hutter.io>

--- common/src/generate.rs
+++ common/src/generate.rs
@@ -17,6 +17,27 @@ where
     input.lines().map(|l| l.parse().unwrap()).collect()
 }
 
+/// Converts a line of comma separated integers with (optional) signs into a list of T's
+///
+/// # Examples
+///
+/// ```
+/// use common::generate::int_list;
+
+/// assert_eq!(vec![3,4,3,1,2], int_list("3,4,3,1,2\n"));
+/// ```
+pub fn int_list<T>(input: &str) -> Vec<T>
+where
+    T: FromStr,
+    <T as FromStr>::Err: Debug,
+{
+    input
+        .trim()
+        .split(',')
+        .map(|e| e.parse().unwrap())
+        .collect()
+}
+
 /// Convert an input into individual characters
 ///
 /// # Examples
