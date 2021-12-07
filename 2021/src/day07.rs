01f2986838b01274ad62cf94ba0ebb588fa18599
Manuel Hutter <manuel@hutter.io>
2021-12-07 09:24:46 +0100

    2021: Day 07 Part 2
    Signed-off-by: Manuel Hutter <manuel@hutter.io>

--- 2021/src/day07.rs
+++ 2021/src/day07.rs
@@ -1,4 +1,4 @@
-pub fn day07p1(input: &[i32]) -> i32 {
+fn count_crabs(input: &[i32]) -> Vec<i32> {
     let max = *input.into_iter().max().unwrap();
     let mut crabs = vec![0; (max + 1) as usize];
 
@@ -8,8 +8,22 @@ pub fn day07p1(input: &[i32]) -> i32 {
         *e += 1;
     }
 
+    crabs
+}
+
+fn cost_for(distance: i32) -> i32 {
+    if distance < 2 {
+        return distance;
+    }
+
+    distance + cost_for(distance - 1)
+}
+
+pub fn day07p1(input: &[i32]) -> i32 {
+    let crabs = count_crabs(input);
+
     let mut lowest = 0;
-    for i in 0..=max {
+    for i in 0..=(crabs.len() as i32) {
         let cost: i32 = crabs
             .iter()
             .enumerate()
@@ -24,7 +38,21 @@ pub fn day07p1(input: &[i32]) -> i32 {
 }
 
 pub fn day07p2(input: &[i32]) -> i32 {
-    input.len() as i32
+    let crabs = count_crabs(input);
+
+    let mut lowest = 0;
+    for i in 0..=(crabs.len() as i32) {
+        let cost: i32 = crabs
+            .iter()
+            .enumerate()
+            .map(|(pos, crabs)| cost_for(((pos as i32) - i).abs()) * crabs)
+            .sum();
+        if cost < lowest || lowest == 0 {
+            lowest = cost;
+        }
+    }
+
+    lowest
 }
 
 #[cfg(test)]
@@ -39,6 +67,15 @@ mod tests {
 
     #[test]
     fn part2_examples() {
-        assert_eq!(0, day07p2(INPUT));
+        assert_eq!(168, day07p2(INPUT));
+    }
+
+    #[test]
+    fn cost_for_distance() {
+        assert_eq!(0, cost_for(0));
+        assert_eq!(1, cost_for(1));
+        assert_eq!(1 + 2, cost_for(2));
+        assert_eq!(1 + 2 + 3, cost_for(3));
+        assert_eq!(1 + 2 + 3 + 4, cost_for(4));
     }
 }
