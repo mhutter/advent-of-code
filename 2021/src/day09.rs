4c6b970be1dc48b140467252549da913b6ed0c17
Manuel Hutter <manuel@hutter.io>
2021-12-09 15:35:07 +0100

    2021: Day 09 Part 2
    Signed-off-by: Manuel Hutter <manuel@hutter.io>

--- 2021/src/day09.rs
+++ 2021/src/day09.rs
@@ -1,32 +1,122 @@
-pub fn day09p1(input: &str) -> u32 {
-    let lines: Vec<Vec<u32>> = input
-        .lines()
-        .map(|l| l.chars().map(|c| c.to_digit(10).unwrap()).collect())
-        .collect();
-    let mut sum = 0;
+use std::collections::HashSet;
 
-    let num_lines = lines.len();
-    let num_rows = lines[0].len();
+struct Heightmap {
+    rows: usize,
+    cols: usize,
 
-    for (l, line) in lines.iter().enumerate() {
-        for (r, val) in line.iter().enumerate() {
-            if (l > 0 && lines[l - 1][r] <= *val)
-                || (l < (num_lines - 1) && lines[l + 1][r] < *val)
-                || (r > 0 && lines[l][r - 1] <= *val)
-                || (r < (num_rows - 1) && lines[l][r + 1] < *val)
-            {
-                continue;
+    map: Vec<Vec<u8>>,
+}
+
+type Position = (usize, usize, u8);
+
+impl From<&str> for Heightmap {
+    fn from(input: &str) -> Self {
+        let map: Vec<Vec<u8>> = input
+            .lines()
+            .map(|l| l.chars().map(|c| c.to_digit(10).unwrap() as u8).collect())
+            .collect();
+
+        let rows = map.len();
+        let cols = map[0].len();
+
+        Self { rows, cols, map }
+    }
+}
+
+impl Heightmap {
+    fn low_points(&self) -> Vec<Position> {
+        let mut positions = Vec::new();
+
+        for (y, row) in self.map.iter().enumerate() {
+            for (x, val) in row.iter().enumerate() {
+                if (y > 0 && self.map[y - 1][x] <= *val)
+                    || ((y + 1) < self.rows && self.map[y + 1][x] < *val)
+                    || (x > 0 && self.map[y][x - 1] <= *val)
+                    || ((x + 1) < self.cols && self.map[y][x + 1] < *val)
+                {
+                    continue;
+                }
+
+                positions.push((x, y, *val));
             }
+        }
 
-            sum += val + 1;
+        positions
+    }
+
+    fn value_at(&self, x: usize, y: usize) -> u8 {
+        self.map[y][x]
+    }
+}
+
+#[derive(Debug)]
+struct Basin {
+    origin: (usize, usize),
+    parts: HashSet<(usize, usize)>,
+}
+
+impl Basin {
+    fn new(x: usize, y: usize) -> Self {
+        Self {
+            origin: (x, y),
+            parts: HashSet::new(),
         }
     }
 
-    sum
+    fn size(&self) -> usize {
+        self.parts.len()
+    }
+
+    fn explore(&mut self, map: &Heightmap) {
+        self.explore_pos(self.origin.0, self.origin.1, map)
+    }
+
+    fn explore_pos(&mut self, x: usize, y: usize, map: &Heightmap) {
+        let pos = (x, y);
+
+        if map.value_at(x, y) > 8 || self.parts.contains(&pos) {
+            // invalid position, not part of any basin or already visited
+            return;
+        }
+        self.parts.insert(pos);
+
+        if x > 0 {
+            self.explore_pos(x - 1, y, map);
+        }
+        if x + 1 < map.cols {
+            self.explore_pos(x + 1, y, map);
+        }
+        if y > 0 {
+            self.explore_pos(x, y - 1, map)
+        }
+        if y + 1 < map.rows {
+            self.explore_pos(x, y + 1, map);
+        }
+    }
 }
 
-pub fn day09p2(input: &str) -> i32 {
-    input.len() as i32
+pub fn day09p1(input: &str) -> u32 {
+    let map = Heightmap::from(input);
+    map.low_points()
+        .into_iter()
+        .map(|(_, _, val)| (val + 1) as u32)
+        .sum()
+}
+
+pub fn day09p2(input: &str) -> usize {
+    let map = Heightmap::from(input);
+    let mut basin_sizes: Vec<usize> = map
+        .low_points()
+        .into_iter()
+        .map(|(x, y, _)| {
+            let mut basin = Basin::new(x, y);
+            basin.explore(&map);
+            basin.size()
+        })
+        .collect();
+
+    basin_sizes.sort();
+    basin_sizes.into_iter().rev().take(3).product()
 }
 
 #[cfg(test)]
@@ -40,7 +130,7 @@ mod tests {
 
     #[test]
     fn part2_examples() {
-        assert_eq!(0, day09p2(INPUT));
+        assert_eq!(1134, day09p2(INPUT));
     }
 
     const INPUT: &str = "2199943210
