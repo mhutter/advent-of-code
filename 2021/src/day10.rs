8f9dd853a017e0ffa0b76a1fd354ecb167333e6c
Manuel Hutter <manuel@hutter.io>
2021-12-10 09:30:37 +0100

    2021: Day 10 Part 2
    Signed-off-by: Manuel Hutter <manuel@hutter.io>

--- 2021/src/day10.rs
+++ 2021/src/day10.rs
@@ -12,8 +12,52 @@ pub fn day10p1(input: &str) -> i32 {
         .sum()
 }
 
-pub fn day10p2(_input: &str) -> i32 {
-    0
+pub fn day10p2(input: &str) -> u64 {
+    let mut scores: Vec<u64> = input
+        .lines()
+        .filter_map(|program| parse(program).ok())
+        .map(|stack| stack.into_iter().rev().map(closing_bracket_for).collect())
+        .map(score_for)
+        .collect();
+
+    scores.sort();
+
+    let n = (scores.len() - 1) / 2;
+    scores[n]
+}
+
+type Stack = Vec<char>;
+type ParseError = char;
+
+fn parse(program: &str) -> Result<Stack, ParseError> {
+    let mut stack = Vec::new();
+    for c in program.chars() {
+        match c {
+            '(' | '[' | '{' | '<' => stack.push(c),
+            ')' | ']' | '}' | '>' => {
+                let top = stack.pop().unwrap();
+                if c != closing_bracket_for(top) {
+                    return Err(c);
+                }
+            }
+            _ => panic!("Unexpected char encountered: {}", c),
+        }
+    }
+
+    Ok(stack)
+}
+
+fn score_for(stack: Vec<char>) -> u64 {
+    stack.into_iter().fold(0, |acc, c| {
+        (acc * 5)
+            + match c {
+                ')' => 1,
+                ']' => 2,
+                '}' => 3,
+                '>' => 4,
+                _ => panic!("Invalid c: {}", &c),
+            }
+    })
 }
 
 fn find_corruption(program: &str) -> Option<char> {
@@ -55,7 +99,29 @@ mod tests {
 
     #[test]
     fn part2_examples() {
-        assert_eq!(0, day10p2(INPUT));
+        assert_eq!(288957, day10p2(INPUT));
+    }
+
+    #[test]
+    fn test_score_for() {
+        assert_eq!(288957, score_for("}}]])})]".chars().collect()));
+        assert_eq!(5566, score_for(")}>]})".chars().collect()));
+        assert_eq!(1480781, score_for("}}>}>))))".chars().collect()));
+        assert_eq!(995444, score_for("]]}}]}]}>".chars().collect()));
+        assert_eq!(294, score_for("])}>".chars().collect()));
+    }
+
+    #[test]
+    fn test_parse() {
+        assert_eq!(
+            Ok(vec!['[', '(', '{', '(', '[', '[', '{', '{']),
+            parse("[({(<(())[]>[[{[]{<()<>>")
+        );
+
+        assert_eq!(Err(']'), parse("(]"));
+        assert_eq!(Err('>'), parse("{()()()>"));
+        assert_eq!(Err('}'), parse("(((()))}"));
+        assert_eq!(Err(')'), parse("<([]){()}[{}])"));
     }
 
     #[test]
@@ -68,6 +134,18 @@ mod tests {
         assert_eq!(Some('>'), find_corruption("{()()()>"));
         assert_eq!(Some('}'), find_corruption("(((()))}"));
         assert_eq!(Some(')'), find_corruption("<([]){()}[{}])"));
+    }
+
+    #[test]
+    fn test_is_corrupted() {
+        for program in valid_programs() {
+            assert!(!is_corrupted(program), "is corrupted: {}", program);
+        }
+
+        assert!(is_corrupted("(]"));
+        assert!(is_corrupted("{()()()>"));
+        assert!(is_corrupted("(((()))}"));
+        assert!(is_corrupted("<([]){()}[{}])"));
     }
 
     fn valid_programs() -> Vec<&'static str> {
