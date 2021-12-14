4a281e4a8f6ef14d350c8dd782b6dfcd1be207e0
Manuel Hutter <manuel@hutter.io>
2021-12-14 17:23:24 +0100

    2021: Day 12 Part 2
    Signed-off-by: Manuel Hutter <manuel@hutter.io>

--- 2021/src/day12.rs
+++ 2021/src/day12.rs
@@ -21,24 +21,29 @@ impl Cave {
     }
 
     /// visits all the linked caves in order, and returns how many times the "end" cave has been reached.
-    fn visit(&self, system: &HashMap<String, Cave>, path: &mut Vec<String>) -> usize {
-        if self.id == "end" {
-            // reached the end, valid path!
-            return 1;
-        }
-
-        if self.small && path.contains(&self.id) {
+    fn visit(
+        &self,
+        system: &HashMap<String, Cave>,
+        path: &mut Vec<String>,
+        can_visit_twice: bool,
+    ) -> usize {
+        if self.small && !can_visit_twice && path.contains(&self.id) {
             // already been here
             return 0;
         }
 
+        let can_visit_twice = can_visit_twice && (!self.small || !path.contains(&self.id));
         path.push(self.id.clone());
         let c = self
             .links
             .iter()
-            .map(|k| {
-                let cave = system.get(k).unwrap();
-                cave.visit(system, path)
+            .map(|k| match k.as_str() {
+                "start" => 0,
+                "end" => 1,
+                _ => {
+                    let cave = system.get(k).unwrap();
+                    cave.visit(system, path, can_visit_twice)
+                }
             })
             .sum();
         path.pop();
@@ -47,7 +52,7 @@ impl Cave {
     }
 }
 
-pub fn day12p1(input: &str) -> usize {
+fn day12(input: &str, can_visit_twice: bool) -> usize {
     let mut system: HashMap<String, Cave> = HashMap::new();
     for line in input.lines() {
         let parts: Vec<&str> = line.split('-').collect();
@@ -67,11 +72,15 @@ pub fn day12p1(input: &str) -> usize {
 
     let mut path = Vec::new();
     let start = system.get("start").unwrap();
-    start.visit(&system, &mut path)
+    start.visit(&system, &mut path, can_visit_twice)
 }
 
-pub fn day12p2(_input: &str) -> usize {
-    0
+pub fn day12p1(input: &str) -> usize {
+    day12(input, false)
+}
+
+pub fn day12p2(input: &str) -> usize {
+    day12(input, true)
 }
 
 #[cfg(test)]
@@ -87,7 +96,9 @@ mod tests {
 
     #[test]
     fn part2_examples() {
-        assert_eq!(0, day12p2(INPUT1));
+        assert_eq!(36, day12p2(INPUT1));
+        assert_eq!(103, day12p2(INPUT2));
+        assert_eq!(3509, day12p2(INPUT3));
     }
 
     const INPUT1: &str = "start-A
