bc90fd47cdba596944077f135f9d782f0a62bbef
Manuel Hutter <manuel@hutter.io>
2021-12-17 20:21:48 +0100

    WIP: 2021: Day 16
    Signed-off-by: Manuel Hutter <manuel@hutter.io>

--- 2021/src/day16.rs
+++ 2021/src/day16.rs
@@ -6,6 +6,13 @@ pub fn day16p2(_input: &str) -> usize {
     0
 }
 
+pub fn decode_hex(input: &str) -> Vec<u8> {
+    let ascii = input.bytes();
+    let mut out = Vec::with_capacity(input.len() / 2);
+
+    out
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -18,6 +25,11 @@ mod tests {
     #[test]
     fn part2_examples() {
         assert_eq!(0, day16p2(INPUT));
+    }
+
+    #[test]
+    fn test_decode_hex() {
+        assert_eq!(vec![] as Vec<u8>, decode_hex(""));
     }
 
     const INPUT: &str = "";
