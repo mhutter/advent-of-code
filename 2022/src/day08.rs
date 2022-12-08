bce4ce141ffc3d7923657e0164407ac194de73f3
Manuel Hutter <manuel@hutter.io>
2022-12-08 21:22:37 +0100

    2022: Day 08 Part 2
    Signed-off-by: Manuel Hutter <manuel@hutter.io>

--- 2022/src/day08.rs
+++ 2022/src/day08.rs
@@ -1,37 +1,85 @@
 use std::collections::HashMap;
 
-pub fn day08p1(input: &str) -> usize {
+type Map = HashMap<(usize, usize), char>;
+type Tree = char;
+
+fn parse(input: &str) -> (Map, usize, usize) {
     // parse data into a HashMap where the coordinates are the key, and the height is the value
-    let map: HashMap<(usize, usize), char> = input
+    let map: HashMap<(usize, usize), Tree> = input
         .lines()
         .enumerate()
-        .flat_map(|(j, row)| row.chars().enumerate().map(move |(i, c)| ((i, j), c)))
+        .flat_map(|(y, row)| row.chars().enumerate().map(move |(x, c)| ((x, y), c)))
         .collect();
 
     // determine dimensions of map
-    let imax = *map.keys().map(|(i, _)| i).max().expect("imax");
-    let jmax = *map.keys().map(|(_, j)| j).max().expect("jmax");
+    let xmax = *map.keys().map(|(x, _)| x).max().expect("imax");
+    let ymax = *map.keys().map(|(_, y)| y).max().expect("jmax");
+
+    (map, xmax, ymax)
+}
+
+pub fn day08p1(input: &str) -> usize {
+    let (map, xmax, ymax) = parse(input);
 
     map.iter()
         // Filter for trees that are visible
-        .filter(|&(&(i, j), &tree)| {
+        .filter(|&(&(x, y), &tree)| {
             // Trees on the edge
-            i == 0
-                || j == 0
-                || i == imax
-                || j == jmax
+            x == 0
+                || y == 0
+                || x == xmax
+                || y == ymax
                 // Trees visible from one side
-                || (0..i).all(|i| *map.get(&(i, j)).unwrap() < tree)
-                || ((i + 1)..=imax).all(|i| *map.get(&(i, j)).unwrap() < tree)
-                || (0..j).all(|j| *map.get(&(i, j)).unwrap() < tree)
-                || ((j + 1)..=jmax).all(|j| *map.get(&(i, j)).unwrap() < tree)
+                || (0..x).all(|x| *map.get(&(x, y)).unwrap() < tree)
+                || ((x + 1)..=xmax).all(|x| *map.get(&(x, y)).unwrap() < tree)
+                || (0..y).all(|y|*map.get(&(x, y)).unwrap() < tree)
+                || ((y + 1)..=ymax).all(|y| *map.get(&(x, y)).unwrap() < tree)
         })
         // count what is left
         .count()
 }
 
-pub fn day08p2(_input: &str) -> usize {
-    0
+trait ViewingDistanceExt {
+    fn viewing_distance_x(self, y: usize, map: &Map, tree: &Tree) -> usize;
+    fn viewing_distance_y(self, x: usize, map: &Map, tree: &Tree) -> usize;
+}
+
+impl<T> ViewingDistanceExt for T
+where
+    T: Iterator<Item = usize> + Clone,
+{
+    fn viewing_distance_x(self, y: usize, map: &Map, tree: &Tree) -> usize {
+        self.clone()
+            .enumerate()
+            .find_map(|(n, x)| map.get(&(x, y)).unwrap().ge(tree).then_some(n + 1))
+            .unwrap_or(self.count())
+    }
+
+    fn viewing_distance_y(self, x: usize, map: &Map, tree: &Tree) -> usize {
+        self.clone()
+            .enumerate()
+            .find_map(|(n, y)| map.get(&(x, y)).unwrap().ge(tree).then_some(n + 1))
+            .unwrap_or(self.count())
+    }
+}
+
+pub fn day08p2(input: &str) -> usize {
+    let (map, xmax, ymax) = parse(input);
+
+    map.iter()
+        // Calculate the "scenic score" for each tree
+        .map(|(&(x, y), tree)| {
+            if x == 0 || x == xmax || y == 0 || y == ymax {
+                return 0;
+            }
+
+            (0..x).rev().viewing_distance_x(y, &map, tree)
+                * ((x + 1)..=xmax).viewing_distance_x(y, &map, tree)
+                * (0..y).rev().viewing_distance_y(x, &map, tree)
+                * ((y + 1)..=ymax).viewing_distance_y(x, &map, tree)
+        })
+        .max()
+        .expect("max score")
 }
 
 #[cfg(test)]
@@ -59,7 +107,7 @@ mod tests {
 
     #[test]
     fn part2_examples() {
-        assert_eq!(0, day08p2(INPUT));
+        assert_eq!(8, day08p2(INPUT));
     }
 
     const INPUT: &str = "30373
