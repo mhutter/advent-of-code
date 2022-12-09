53371774ede49f49102d330fc30dca0d276dba28
Manuel Hutter <manuel@hutter.io>
2022-12-09 12:57:54 +0100

    2022: Day 09 Part 2
    Signed-off-by: Manuel Hutter <manuel@hutter.io>

--- 2022/src/day09.rs
+++ 2022/src/day09.rs
@@ -37,14 +37,13 @@ mod rope {
                 return Some(self.current_dir.expect("a direction for remaining moves"));
             }
 
-            if let Some(line) = self.lines.next() {
+            self.lines.next().map(|line| {
                 let (dir, amt) = line.split_once(' ').expect("whitespace");
-                self.current_dir = Some(Direction::from(dir));
+                let dir = Direction::from(dir);
+                self.current_dir = Some(dir);
                 self.remaining = amt.parse::<usize>().expect("number") - 1;
-                return self.current_dir;
-            }
-
-            None
+                dir
+            })
         }
     }
 
@@ -67,14 +66,14 @@ mod rope {
         }
     }
 
-    #[derive(Debug, Default)]
+    #[derive(Default, Clone, Copy)]
     pub struct End {
         x: isize,
         y: isize,
     }
-    impl std::fmt::Display for End {
+    impl std::fmt::Debug for End {
         fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-            write!(f, "{}/{}", self.x, self.y)
+            write!(f, "({}/{})", self.x, self.y)
         }
     }
     impl End {
@@ -91,66 +90,60 @@ mod rope {
             };
         }
 
-        /// Move `self` towards `other`, respecting the rules outlined in
+        /// Move `self` towards `head`, respecting the rules outlined in
         /// https://adventofcode.com/2022/day/9
-        pub fn move_towards(&mut self, other: &Self) {
-            let dx = other.x - self.x;
-            let dy = other.y - self.y;
+        pub fn move_towards(&mut self, head: &Self) {
+            let dx = head.x - self.x;
+            let dy = head.y - self.y;
 
             // If we're within 1 of `other`, no movement is required
             if dx.abs() <= 1 && dy.abs() <= 1 {
                 return;
             }
 
-            //
-            match (dx, dy) {
-                // movement on 1 axis
-                (2, 0) => self.x += 1,
-                (0, 2) => self.y += 1,
-                (-2, 0) => self.x -= 1,
-                (0, -2) => self.y -= 1,
-                // if there was a diagonal movement, one of the deltas must be 1 or -1, and the
-                // other must be 2 or -2.
-                (2, _) => {
-                    self.x += 1;
-                    self.y = other.y
-                }
-                (_, 2) => {
-                    self.y += 1;
-                    self.x = other.x;
-                }
-                (-2, _) => {
-                    self.x -= 1;
-                    self.y = other.y;
-                }
-                (_, -2) => {
-                    self.y -= 1;
-                    self.x = other.x;
-                }
-                // other cases do not exist according to problem statement
-                _ => panic!("{self} {other}"),
-            }
+            // movement is limited, so our goal is to reduce dx and dy by 1 each
+            self.x += dx.signum();
+            self.y += dy.signum();
         }
     }
 }
 
 pub fn day09p1(input: &str) -> usize {
-    let mut visited = HashSet::from([(0, 0)]);
     let (mut head, mut tail) = (End::default(), End::default());
 
-    Movements::from(input).for_each(|direction| {
-        debug!("BEFORE at {} {}", &head, &tail);
-        head.move_step(direction);
-        tail.move_towards(&head);
-        visited.insert(tail.coords());
-        debug!("AFTER  at {} {}", &head, &tail);
-    });
+    Movements::from(input)
+        .map(|direction| {
+            debug!("BEFORE at {head:?} {tail:?}");
+            head.move_step(direction);
+            tail.move_towards(&head);
+            debug!("AFTER  at {head:?} {tail:?}");
 
-    visited.len()
+            tail.coords()
+        })
+        .collect::<HashSet<_>>()
+        .len()
 }
 
-pub fn day09p2(_input: &str) -> usize {
-    0
+pub fn day09p2(input: &str) -> usize {
+    let mut knots = [End::default(); 10];
+
+    Movements::from(input)
+        .map(|direction| {
+            debug!("BEFORE {knots:?}");
+
+            // move the head
+            knots[0].move_step(direction);
+
+            // move rest
+            for i in 1..(knots.len()) {
+                knots[i].move_towards(&knots[i - 1].clone());
+            }
+
+            debug!("AFTER  {knots:?}");
+            knots.last().unwrap().coords()
+        })
+        .collect::<HashSet<_>>()
+        .len()
 }
 
 #[cfg(test)]
@@ -164,7 +157,9 @@ mod tests {
 
     #[test]
     fn part2_examples() {
-        assert_eq!(0, day09p2(INPUT));
+        env_logger::builder().is_test(true).init();
+        assert_eq!(1, day09p2(INPUT), "small input");
+        assert_eq!(36, day09p2(INPUT2), "big input");
     }
 
     const INPUT: &str = "R 4
@@ -175,4 +170,13 @@ R 4
 D 1
 L 5
 R 2";
+
+    const INPUT2: &str = "R 5
+U 8
+L 8
+D 3
+R 17
+D 10
+L 25
+U 20";
 }
