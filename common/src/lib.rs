35d5b13e347b530ff2a0b18e0c1420a805b1d8b6
Manuel Hutter <manuel@hutter.io>
2022-12-17 15:12:48 +0100

    feat(common): ExpectPrefixExt
    Signed-off-by: Manuel Hutter <manuel@hutter.io>

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -1,1 +1,11 @@
 pub mod generate;
+
+pub trait ExpectPrefixExt {
+    fn expect_prefix(self, prefix: &str) -> Self;
+}
+
+impl ExpectPrefixExt for &str {
+    fn expect_prefix(self, prefix: &str) -> Self {
+        self.strip_prefix(prefix).expect(prefix)
+    }
+}
