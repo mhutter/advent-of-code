49b0eb38c80491a268fad6978c9fa2694d9b9152
Manuel Hutter <manuel@hutter.io>
2024-01-08 14:07:08 +0100

    2023: Day 01 Part 1
    Signed-off-by: Manuel Hutter <manuel@hutter.io>

--- 2023/src/day01.rs
+++ 2023/src/day01.rs
@@ -1,5 +1,16 @@
-pub fn day01p1(_input: &str) -> u32 {
-    0
+pub fn day01p1(input: &str) -> u32 {
+    input
+        .lines()
+        .map(|line| {
+            let digits = line
+                .chars()
+                .filter(char::is_ascii_digit)
+                .map(|c| c as u8 - b'0')
+                .collect::<Vec<_>>();
+
+            (digits.first().unwrap() * 10 + digits.last().unwrap()) as u32
+        })
+        .sum()
 }
 
 pub fn day01p2(_input: &str) -> u32 {
@@ -12,13 +23,16 @@ mod tests {
 
     #[test]
     fn part1_examples() {
-        assert_eq!(0, day01p1(INPUT));
+        const INPUT: &str = "1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet";
+        assert_eq!(142, day01p1(INPUT));
     }
 
     #[test]
     fn part2_examples() {
+        const INPUT: &str = "";
         assert_eq!(0, day01p2(INPUT));
     }
-
-    const INPUT: &str = "";
 }
