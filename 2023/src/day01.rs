1927ff1d02f7d64d177b8a806f8587e998d2c7d3
Manuel Hutter <manuel@hutter.io>
2024-01-08 14:39:56 +0100

    2023: Day 01 Part 2
    Signed-off-by: Manuel Hutter <manuel@hutter.io>

--- 2023/src/day01.rs
+++ 2023/src/day01.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 pub fn day01p1(input: &str) -> u32 {
     input
         .lines()
@@ -13,8 +15,34 @@ pub fn day01p1(input: &str) -> u32 {
         .sum()
 }
 
-pub fn day01p2(_input: &str) -> u32 {
-    0
+pub fn day01p2(input: &str) -> u32 {
+    const WORDS: [&str; 9] = [
+        "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+    ];
+
+    // build lookup table
+    let words = WORDS
+        .iter()
+        .enumerate()
+        .map(|(i, &w)| (w, (i + 1).to_string()))
+        .collect::<HashMap<_, _>>();
+
+    // convienience
+    #[inline]
+    fn starts_with_word(i: &str) -> Option<&'static str> {
+        WORDS.iter().find(|&w| i.starts_with(w)).copied()
+    }
+
+    // replace all words by digits
+    let mut input = String::from(input);
+    // longest word: 5 letters
+    for i in 0..(input.len() - 5) {
+        if let Some(word) = starts_with_word(&input[i..]) {
+            input.replace_range(i..(i + 1), words.get(word).unwrap());
+        }
+    }
+
+    day01p1(&input)
 }
 
 #[cfg(test)]
@@ -32,7 +60,13 @@ treb7uchet";
 
     #[test]
     fn part2_examples() {
-        const INPUT: &str = "";
-        assert_eq!(0, day01p2(INPUT));
+        const INPUT: &str = "two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen";
+        assert_eq!(281, day01p2(INPUT));
     }
 }
