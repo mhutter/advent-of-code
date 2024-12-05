570db99ec9bf2c2e59d334131c5a6cf3555f6e38
Manuel Hutter <manuel@hutter.io>
2024-12-05 08:34:16 +0100

    2024: Day 04 Part 1
    Signed-off-by: Manuel Hutter <manuel@hutter.io>

--- 2024/src/day04.rs
+++ 2024/src/day04.rs
@@ -1,5 +1,72 @@
-pub fn day04p1(_input: &str) -> u32 {
-    0
+pub fn day04p1(input: &str) -> u32 {
+    let input: Vec<Vec<char>> = input.lines().map(|l| l.chars().collect()).collect();
+    let rows = input.len();
+    let cols = input[0].len();
+
+    let mut sum = 0;
+
+    // Directions
+    // - right
+    // - left
+    // - down
+    // - up
+    // - bottom-right
+    // - bottom-left
+    // - top-right
+    // - top-left
+
+    // Check left-to-right and right-to-left
+    for row in input.iter() {
+        for col in 0..(cols - 3) {
+            match row[col..(col + 4)] {
+                ['X', 'M', 'A', 'S'] | ['S', 'A', 'M', 'X'] => sum += 1,
+                _ => {}
+            }
+        }
+    }
+    // expected: 5
+
+    // Check downward and upward
+    for row in 0..(rows - 3) {
+        for col in 0..cols {
+            let candidate = (row..(row + 4)).map(|r| input[r][col]).collect::<Vec<_>>();
+            match candidate.as_slice() {
+                ['X', 'M', 'A', 'S'] | ['S', 'A', 'M', 'X'] => sum += 1,
+                _ => {}
+            }
+        }
+    }
+    // expected: 3
+
+    // check top-left to bottom-right
+    for row in 0..(rows - 3) {
+        for col in 0..(cols - 3) {
+            let candidate = (row..(row + 4))
+                .zip(col..(col + 4))
+                .map(|(r, c)| input[r][c])
+                .collect::<Vec<_>>();
+            match candidate.as_slice() {
+                ['X', 'M', 'A', 'S'] | ['S', 'A', 'M', 'X'] => sum += 1,
+                _ => {}
+            }
+        }
+    }
+
+    // top-right to bottom-left
+    for row in 0..(rows - 3) {
+        for col in 0..(cols - 3) {
+            let candidate = (row..(row + 4))
+                .zip((col..(col + 4)).rev())
+                .map(|(r, c)| input[r][c])
+                .collect::<Vec<_>>();
+            match candidate.as_slice() {
+                ['X', 'M', 'A', 'S'] | ['S', 'A', 'M', 'X'] => sum += 1,
+                _ => {}
+            }
+        }
+    }
+
+    sum
 }
 
 pub fn day04p2(_input: &str) -> u32 {
@@ -12,7 +79,7 @@ mod tests {
 
     #[test]
     fn part1_examples() {
-        assert_eq!(0, day04p1(INPUT));
+        assert_eq!(18, day04p1(INPUT));
     }
 
     #[test]
@@ -20,5 +87,14 @@ mod tests {
         assert_eq!(0, day04p2(INPUT));
     }
 
-    const INPUT: &str = "";
+    const INPUT: &str = "MMMSXXMASM
+MSAMXMSMSA
+AMXSXMAAMM
+MSAMASMSMX
+XMASAMXAMM
+XXAMMXXAMA
+SMSMSASXSS
+SAXAMASAAA
+MAMMMXMMMM
+MXMXAXMASX";
 }
